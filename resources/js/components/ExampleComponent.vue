4f315735d9e3e6c2dbe012dbc31bb761ac08cd97
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-10-01 17:24:41 -0300

    Aula 3

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        HUE BR XABLAU da zoeira
                     </div>
                 </div>
             </div>
