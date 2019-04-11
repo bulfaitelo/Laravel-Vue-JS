29fedb2dfa850355b6ecad5a1bb535b0608e45ff
Thiago Rodrigues <bulfaitelo@gmail.com>
2019-04-11 17:19:27 -0300

    Aula 12

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,7 +3,66 @@
 @section('content')
 <pagina tamanho="12">
     <painel titulo="Lista de Artigos">
-                 
+        <a href="#">Criar</a>                 
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Titulo ...</td>
+                    <td>xablau</td>
+                    <td>Thiago</td>
+                    <td>11/04/2019</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>                    
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Titulo ...</td>
+                    <td>xablau</td>
+                    <td>Thiago</td>
+                    <td>11/04/2019</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>                    
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Titulo ...</td>
+                    <td>xablau</td>
+                    <td>Thiago</td>
+                    <td>11/04/2019</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>                    
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Titulo ...</td>
+                    <td>xablau</td>
+                    <td>Thiago</td>
+                    <td>11/04/2019</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>                    
+                </tr>
+                
+            </tbody>
+        </table>
     </painel>
 </pagina>
 @endsection
